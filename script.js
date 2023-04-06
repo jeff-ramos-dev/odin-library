763c601758dcd6df8d8c6821f2adc9c2ff8680d8
Jeff Ramos <103221133+jeff-ramos-dev@users.noreply.github.com>
2023-04-06 09:15:59 -0700

    Changed from books to movies

--- script.js
+++ script.js
@@ -1,50 +1,56 @@
-function Book(title, author, pages, read = false) {
+function Movie(title, director, runtime, year, watched = false) {
   this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.runtime = runtime
+  this.director = director;
+  this.year = year;
+  this.watched = watched;
 
-  Book.prototype.info = () =>
-    `${this.title} by ${this.author}, ${this.pages} pages, ${
-      read ? 'read' : 'not read yet'
+  Movie.prototype.info = () =>
+    `${this.title} by ${this.director}, ${this.runtime} minutes, released ${year}, ${
+      watched ? 'watched' : 'not not watched yet'
     }`;
 }
 
 const library = [];
 
-const books = document.querySelector('.books');
-const bookForm = document.querySelector('.book-form')
-const addBtn = document.querySelector('.new-book');
+const movies = document.querySelector('.movies');
+const movieForm = document.querySelector('.movie-form');
+const addBtn = document.querySelector('.new-movie');
 
-const theHobbit = new Book('The Hobbit', 'J. R. R. Tolkien', 295);
-const harryPotter = new Book(
-  "Harry Potter and the Sorcerer's Stone",
-  'J. K. Rowling',
-  452,
-  true
-);
-const theStand = new Book('The Stand', 'Stephen King', 1050);
+const theMatrix = new Movie('The Matrix', 'Wacowski Brothers', 100, 2000, true);
+const shawshankRedemption = new Movie('Shawshank Redepmtion', 'Stephen Spielberg', 160, 1992, true);
+const thePrestige = new Movie('The Prestige', 'Christopher Nolan', 125, 2006, true);
+const theLionKing = new Movie('The Lion King', 'Disney', 90, 1992, true);
 
 addBtn.addEventListener('click', () => {
-  bookForm.classList.toggle('hidden')  
-})
+  movieForm.classList.toggle('hidden');
+});
 
-function addBookToLibrary(book) {
-  const newBook = new Book(book.title, book.author, book.pages, book.read);
-  library.push(newBook);
+function addMovieToLibrary(movie) {
+  const newMovie = new Movie(movie.title, movie.director, movie.runtime, movie.year, movie.watched);
+  library.push(newMovie);
 }
 
-addBookToLibrary(theHobbit);
-addBookToLibrary(harryPotter);
-addBookToLibrary(theStand);
+addMovieToLibrary(theMatrix);
+addMovieToLibrary(shawshankRedemption);
+addMovieToLibrary(thePrestige);
+addMovieToLibrary(theLionKing);
 
 function displayLibrary() {
   for (let i = 0; i < library.length; i++) {
-    const text = `${library[i].title} by ${library[i].author}, ${library[i].pages} pages. ${library[i].read ? 'read' : 'unread'}`;
-    const currentBook = document.createElement('li');
-    currentBook.textContent = text;
-    currentBook.classList.add('book');
-    books.appendChild(currentBook);
+    const currentMovie = document.createElement('tr');
+    const currentMovieTitle = document.createElement('td');
+    const currentMovieDirector = document.createElement('td');
+    const currentMovieRuntime = document.createElement('td');
+    const currentMovieYear = document.createElement('td');
+    const currentMovieWatched = document.createElement('td')
+    currentMovieTitle.textContent = library[i].title;
+    currentMovieDirector.textContent = library[i].director;
+    currentMovieRuntime.textContent = library[i].runtime;
+    currentMovieYear.textContent = library[i].year;
+    currentMovieWatched.textContent = library[i].watched;
+    currentMovie.classList.add('movie');
+    movies.appendChild(currentMovie);
   }
 }
 
